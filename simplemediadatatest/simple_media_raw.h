96bd1c93a861c8c672ae8949a1fd98d89a5787f8
Alvin2010 <709699023@qq.com>
2017-12-26 22:41:00 +0800

    pcm

--- simplemediadatatest/simple_media_raw.h
+++ simplemediadatatest/simple_media_raw.h
@@ -1,5 +1,5 @@
 
-
+//video
 void simple_yuv420p_split(const char* url, int w, int h, int num);
 
 void simple_yuv422p_split(const char* url, int w, int h, int num);
@@ -23,3 +23,18 @@ void simple_rgb24_to_bmp(const char *rgb24path, int width, int height, const cha
 void simple_rgb24_to_yuv420(const char *rgb24path, int width, int height, const char *yuv420path);
 
 void simple_rgb24_colorbar(int width, int height, char *url_out);
+
+
+
+//audio
+void simple_pcm16le_split(const char* url);
+
+void simple_pcm16le_halfvolumeleft(const char* url);
+
+void simple_pcm16le_doublespeed(const char* url);
+
+void simple_pcm16le_to_pcm8(const char* url);
+
+void simple_pcm16le_cut_singlechannel(const char* url, int startpos, int num);
+
+void simple_pcm16le_to_wav(const char *pcmpath, int channels, int sample_rate, const char *wavepath);
